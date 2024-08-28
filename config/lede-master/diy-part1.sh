af421d93c275d4520a70c9e4533e7ef11a38ffca
Neko <165999598+ArminYu@users.noreply.github.com>
2024-08-28 16:58:57 +0800

    Update diy-part1.sh

--- config/lede-master/diy-part1.sh
+++ config/lede-master/diy-part1.sh
@@ -6,20 +6,12 @@
 # Source code repository: https://github.com/coolsnowwolf/lede / Branch: master
 #========================================================================================================================
 
+# 删除源码的adguardhome
+sed -i '/files/adguardhome.*/d' xxxx/packages/net/adguardhome/Makefile
+sed -i '/$(INSTALL_DIR) $(1)/etc/d' xxxx/packages/net/adguardhome/Makefile
+
 # Add a feed source
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 echo 'src-git Openclash https://github.com/vernesong/OpenClash' >>feeds.conf.default
 echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
 
-# other
-# 删除自定义源默认的 argon 主题
-rm -rf package/lean/luci-theme-argon
-
-# 部分第三方源自带 argon 主题，上面命令删除不掉的请运行下面命令
-find ./ -name luci-theme-argon | xargs rm -rf;
-
-# 针对 LEDE 项目拉取 argon 原作者的源码
-git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
-
-# 替换默认主题为 luci-theme-argon
-sed -i 's/luci-theme-bootstrap/luci-theme-argon/' feeds/luci/collections/luci/Makefile
