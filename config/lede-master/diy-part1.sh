e1f8d8e6b3008c05d10e03803fa3351e5e797793
Neko <165999598+ArminYu@users.noreply.github.com>
2024-08-28 16:18:22 +0800

    Update diy-part1.sh

--- config/lede-master/diy-part1.sh
+++ config/lede-master/diy-part1.sh
@@ -12,5 +12,14 @@ echo 'src-git Openclash https://github.com/vernesong/OpenClash' >>feeds.conf.def
 echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
 
 # other
-# rm -rf package/lean/{samba4,luci-app-samba4,luci-app-ttyd}
+# 删除自定义源默认的 argon 主题
+rm -rf package/lean/luci-theme-argon
 
+# 部分第三方源自带 argon 主题，上面命令删除不掉的请运行下面命令
+find ./ -name luci-theme-argon | xargs rm -rf;
+
+# 针对 LEDE 项目拉取 argon 原作者的源码
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
+
+# 替换默认主题为 luci-theme-argon
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/' feeds/luci/collections/luci/Makefile
