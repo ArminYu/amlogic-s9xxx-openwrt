23b3166242f93a9eb0d23ef57ad18c7b292e5790
Neko <165999598+ArminYu@users.noreply.github.com>
2024-08-29 00:13:18 +0800

    Update diy-part2.sh

--- config/lede-master/diy-part2.sh
+++ config/lede-master/diy-part2.sh
@@ -7,8 +7,7 @@
 #========================================================================================================================
 
 # ------------------------------- Main source started -------------------------------
-#
-# other
+
 # 删除自定义源默认的 argon 主题
 rm -rf package/lean/luci-theme-argon
 
@@ -18,8 +17,8 @@ find ./ -name luci-theme-argon | xargs rm -rf;
 # 针对 LEDE 项目拉取 argon 原作者的源码
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
 
-# 替换默认主题为 luci-theme-argon
-sed -i 's/luci-theme-bootstrap/luci-theme-argon/' feeds/luci/collections/luci/Makefile
+# Modify default theme（FROM uci-theme-bootstrap CHANGE TO luci-theme-argon）
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 
 # Add autocore support for armvirt
 sed -i 's/TARGET_rockchip/TARGET_rockchip\|\|TARGET_armvirt/g' package/lean/autocore/Makefile
