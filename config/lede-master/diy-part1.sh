8c6419370648d93962c9daf0da90d8dd5795ae95
Neko <165999598+ArminYu@users.noreply.github.com>
2024-08-28 23:29:42 +0800

    Update diy-part1.sh

--- config/lede-master/diy-part1.sh
+++ config/lede-master/diy-part1.sh
@@ -6,11 +6,12 @@
 # Source code repository: https://github.com/coolsnowwolf/lede / Branch: master
 #========================================================================================================================
 
-# 删除源码的adguardhome
-sed -i -e 's/./files/adguardhome.init //g' -e 's/./files/adguardhome.config //g' xxxx/packages/net/adguardhome/Makefile
-
 # Add a feed source
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 echo 'src-git Openclash https://github.com/vernesong/OpenClash' >>feeds.conf.default
 echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
+
+# other
+rm -rf feeds/packages/net/{adguardhome}
+rm -rf package/feeds/packages/{adguardhome}
 
