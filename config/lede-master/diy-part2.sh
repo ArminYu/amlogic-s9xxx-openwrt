b506e855e9ce9f1360d31b669e554c4b0dfdaf8c
Neko <165999598+ArminYu@users.noreply.github.com>
2024-08-29 11:02:36 +0800

    Update diy-part2.sh

--- config/lede-master/diy-part2.sh
+++ config/lede-master/diy-part2.sh
@@ -12,7 +12,7 @@
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 
 # 更改 Argon 主题背景
-cp -f $GITHUB_WORKSPACE/images/bg1.jpg package/luci-theme-argon/htdocs/luci-static/argon/img/bg1.jpg
+# cp -f $GITHUB_WORKSPACE/images/bg1.jpg package/luci-theme-argon/htdocs/luci-static/argon/img/bg1.jpg
 
 # Add autocore support for armvirt
 sed -i 's/TARGET_rockchip/TARGET_rockchip\|\|TARGET_armvirt/g' package/lean/autocore/Makefile
